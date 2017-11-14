49ecdc009271091528a7b25ed7b518ddd5d2e419
Zachariah Lowe <lowezake@gmail.com>
2017-11-14 13:15:09 -0700

    inital

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import NavBar from './NavBar'
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <NavBar />
+      </div>
+    );
+  }
+}
+
+export default App;
